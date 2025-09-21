07229d978ba788b9a2ffda7bc903a0c826bb8b89
Nazhik123 <novozhilov_dp@mail.ru>
2025-09-21 22:30:25 +0300

    Добавьте файлы проекта.

---
+++ lab1/5/5.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>;
+using namespace std;
+int main() {
+	float decpounds;
+	cin >> decpounds; //Ввожу десятичные фунты
+	float pounds,decfrac,pens,frac;
+	pounds = static_cast<int>(decpounds); //Беру целую часть от них
+	decfrac = (decpounds - pounds) * 20;  //Считаю десятичные шиллинги
+	frac = static_cast<int>(decfrac);	  //Беру целую часть от них
+	pens = static_cast<int>((decfrac - frac) * 12); //Считаю пенсы
+	
+	cout << pounds << "." << frac << "." << pens; //Вывожу получившие величины через точки
+}
---
+++ lab1/ConsoleApplication1/2.cpp
@@ -0,0 +1,9 @@
+﻿#include<iostream>;
+using namespace std;
+int main() {
+	float c;
+	float f;
+	cin >> c; //Ввожу градусы по Цельсию
+	f = c * 9 / 5 + 32; //Перевожу в градусы по Фаренгейту
+	cout << f; //Вывожу градусы по Фаренгейту
+}
