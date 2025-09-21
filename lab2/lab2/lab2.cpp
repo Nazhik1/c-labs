c76f5ccc6f618d960abbae9a3b28e83a6181635c
Nazhik123 <novozhilov_dp@mail.ru>
2025-09-21 23:54:30 +0300

    add lab2

---
+++ lab2/lab2/lab2.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>;
+#include<string>;
+using namespace std;
+
+int main() {
+	int n = 1;
+	for (n; n != 21; n++) {
+		char ch = ' ';
+		string num1(20 - n, ch);
+		string num2(n, 'x');
+		string res = (num1 + num2)+ "\n";
+		cout << res << endl;
+	}
+}
