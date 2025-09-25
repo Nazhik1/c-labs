d9cd6902ef332ebd72cfd2851ac82c1c77c88ae6
Nazhik123 <novozhilov_dp@mail.ru>
2025-09-25 15:43:09 +0300

    lab3 n1 n2

---
+++ lab3/1/1.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>;
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	struct employee { int n; float z; };
+	employee emp1, emp2, emp3;
+	cout << "Введите данные трёх сотрудников: " << endl;
+	cin >> emp1.n >> emp1.z;
+	cin >> emp2.n >> emp2.z;
+	cin >> emp3.n >> emp3.z;
+	cout << "Данные первого сотрудника: " << emp1.n << ' ' << emp1.z << endl;
+	cout << "Данные второго сотрудника: " << emp2.n << ' ' << emp2.z << endl;
+	cout << "Данные третьего сотрудника: " << emp3.n << ' ' << emp3.z << endl;
+}
---
+++ lab3/2/2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main(){   
+    setlocale(LC_ALL, "RUS");
+    enum etype { laborer = 'l', secretary = 's', manager = 'm', accountant='a', executive='e', researcher='r' };
+    char a;
+    cout << "Введите первую букву должности: ";
+    cin >> a;
+    switch (a)
+    {
+        case etype::laborer:
+            cout << "Полное название: laborer";
+            break;
+        case etype::secretary:
+            cout << "Полное название: secretary";
+            break;
+        case etype::manager:
+            cout << "Полное название: manager";
+            break;
+        case etype::accountant:
+            cout << "Полное название: accountant";
+            break;
+        case etype::executive:
+            cout << "Полное название: executive";
+            break;
+        case etype::researcher:
+            cout << "Полное название: researcher";
+            break;
+    }
+}
