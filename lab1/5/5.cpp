7843c41c016cc9c9420df869d98ee3359da7e7d5
Nazhik123 <novozhilov_dp@mail.ru>
2025-09-26 15:28:51 +0300

    done

--- lab1/5/5.cpp
+++ lab1/5/5.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>;
 using namespace std;
 int main() {
+	setlocale(LC_ALL, "RUS");
 	float decpounds;
+	cout << "Введите дес. фунты: ";
 	cin >> decpounds; //Ввожу десятичные фунты
 	float pounds,decfrac,pens,frac;
 	pounds = static_cast<int>(decpounds); //Беру целую часть от них
@@ -9,5 +11,5 @@ int main() {
 	frac = static_cast<int>(decfrac);	  //Беру целую часть от них
 	pens = static_cast<int>((decfrac - frac) * 12); //Считаю пенсы
 	
-	cout << pounds << "." << frac << "." << pens; //Вывожу получившие величины через точки
+	cout << "Старые фунты: " << pounds << "." << frac << "." << pens; //Вывожу получившие величины через точки
 }
