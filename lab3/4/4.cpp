00fdbe64e6894880e4035a823dbefc22ee19d5bb
Nazhik123 <novozhilov_dp@mail.ru>
2025-10-10 17:42:26 +0300

    n4

---
+++ lab3/4/4.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "RUS");
+	struct time { int hours; int minutes; int seconds; };
+	time t;
+	cout << "Введите время: ";
+	cin >> t.hours >> t.minutes >> t.seconds;
+	long totalsecs = t.hours * 3600 + t.minutes * 60 + t.seconds;
+	cout << "Кол-во секунд: " << totalsecs;
+}
