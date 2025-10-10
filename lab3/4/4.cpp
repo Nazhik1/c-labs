df536cd7ce761a304312a71dc9117586a34fe05e
Nazhik123 <novozhilov_dp@mail.ru>
2025-10-10 18:02:47 +0300

    end

--- lab3/4/4.cpp
+++ lab3/4/4.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main(){
 	setlocale(LC_ALL, "RUS");
-	struct time { int hours; int minutes; int seconds; };
+	struct time { int hours; int minutes; int seconds; }; //Создаём структуру для времени
 	time t;
 	cout << "Введите время: ";
-	cin >> t.hours >> t.minutes >> t.seconds;
-	long totalsecs = t.hours * 3600 + t.minutes * 60 + t.seconds;
+	cin >> t.hours >> t.minutes >> t.seconds; //Вводим время в виде структуры
+	long totalsecs = t.hours * 3600 + t.minutes * 60 + t.seconds; //Считаем секунды и выводим
 	cout << "Кол-во секунд: " << totalsecs;
 }
--- lab3/5/5.cpp
+++ lab3/5/5.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 int main(){
     setlocale(LC_ALL, "RUS");
-    struct time { int hours; int minutes; int seconds; };
+    struct time { int hours; int minutes; int seconds; }; //Создаём структуру времени
     time t1, t2, t3;
     char ch;
     cout << "Введите первое время: ";
-    cin >> t1.hours >> ch >> t1.minutes >> ch >> t1.seconds;
+    cin >> t1.hours >> ch >> t1.minutes >> ch >> t1.seconds; // Вводим 2 времени 
     cout << "Введите второе время: ";
     cin >> t2.hours >> ch >> t2.minutes >> ch >> t2.seconds;
-    long totalsecs1 = t1.hours * 3600 + t1.minutes * 60 + t1.seconds;
+    long totalsecs1 = t1.hours * 3600 + t1.minutes * 60 + t1.seconds; //Переводим их в секунды
     long totalsecs2 = t2.hours * 3600 + t2.minutes * 60 + t2.seconds;
-    long totalsecs = totalsecs1 + totalsecs2;
+    long totalsecs = totalsecs1 + totalsecs2; //Считаем сумму времени и переводим обратно в формат структуры
     t3.seconds = totalsecs % 60;
     t3.minutes = (totalsecs / 60) % 60;
     t3.hours = totalsecs / 3600;
-    cout << "Суммарное время: " << t3.hours << ':' << t3.minutes << ':' << t3.seconds
+    cout << "Суммарное время: " << t3.hours << ':' << t3.minutes << ':' << t3.seconds; // Выводим результат через структуру
 }
