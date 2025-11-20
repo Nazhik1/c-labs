7a96fccdaa59ee95c66157fdcfe4a65dc4093908
Nazhik123 <novozhilov_dp@mail.ru>
2025-11-20 19:27:55 +0300

    lab 5 2

---
+++ lab5/2/2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>;
+using namespace std;
+
+void maxint(int a[], int size) {
+	int max = 0;
+	int max_i=0;
+	for (int i = 0; i < size; i++) {
+		if (a[i] > max) {
+			max = a[i];
+			max_i = i;
+		}
+	}
+	cout << max << endl << max_i;
+}
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int size;
+	cout << "Введите кол-во элементов:" << endl;
+	cin >> size;
+	int* a = new int[size];
+	cout << "Введите элементы: " << endl;
+	for (int i = 0; i < size; i++) {
+		cin >> a[i];
+	}
+	maxint(a, size);
+}
